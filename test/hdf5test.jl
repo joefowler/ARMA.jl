8e3413d4d1d5831faed4ec857762c9e1246eb9da
Joe Fowler <drjoefowler@gmail.com>
2018-01-17 16:28:22 -0700

    Here's a problem model that needs to store/load as HDF5

--- test/hdf5test.jl
+++ test/hdf5test.jl
@@ -36,3 +36,30 @@ end
         end
     end
 end #testset
+
+@testset "hdf5 save/load complex" begin
+    # These specific bases/amplitudes gave me problems before
+    bases = [0.4501949250518989 - 0.8928250258588964im, 0, .6922098389057583, 0.9713167531173473, 0.9834786370247353]
+    bases[2] = conj(bases[1])
+    ampls = [5.7138922321940395 + 0.9683112174170238im, 0, 5.883653745922931, 30.940176242772605, 12.743102625756544]
+    ampls[2] = conj(ampls[1])
+    covarIV = [195.135]
+    model = ARMAModel(bases, ampls, covarIV)
+
+    fname1 = tempname()*".hdf5"
+    ARMA.hdf5save(fname1, model)
+    model1 = ARMA.hdf5load(fname1)
+    compare(model, model1)
+
+    fname2 = tempname()*".hdf5"
+    h5open(fname2, "w") do f
+        g1 = g_create(f, "top")
+        g2 = g_create(g1, "mid")
+        g3 = g_create(g2, "low")
+        ARMA.hdf5save(g3, model)
+    end
+    h5open(fname2, "r") do f
+        model2 = ARMA.hdf5load(f["top/mid/low"])
+        compare(model, model2)
+    end
+end
