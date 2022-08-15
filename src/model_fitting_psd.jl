180f5a2a8cfed10d8e2a69c0ca25c27fb2e4acee
Joe Fowler <drjoefowler@gmail.com>
2022-08-15 12:53:20 -0600

    False start on fitting PSD?

--- src/model_fitting_psd.jl
+++ src/model_fitting_psd.jl
@@ -3,6 +3,7 @@
 #
 
 using OffsetArrays
+using LsqFit
 
 """
     innovations_estimate(autocorr::AbstractVector, p::Integer, q=-1)
@@ -17,15 +18,15 @@ the equivalent mixed ARMA model, with coefficients θ and ϕ for the MA and AR p
 
 # Arguments
 - `autocorr`: The ARMA process autocorrelation function, as estimated from data.
-- `p`: The AR order of the ARMA process.
-- `q`: The MA order of the ARMA process (if `q` is not given, then let `q=p`).
+- `p`: The AR order of the ARMA process (p≥0).
+- `q`: The MA order of the ARMA process (q≥0; if `q` is not given or is negative, then let `q=p`).
 
 # Returns
 
     (psd,θ,ϕ)
-- `psd` is a scalar function returning the power spectrum at the given angular frequency
+- `psd` is a scalar function returning the power spectrum at the given angular frequency in [0,π]
 - `θ` is the MA polynomial
-- `ϕ` is the AR polynomial
+- `ϕ` is the AR polynomial (with normalization ϕ[0]=1)
 
 """
 function innovations_estimate(autocorr::AbstractVector, p::Integer, q=-1)
@@ -59,8 +60,8 @@ function innovations_estimate(autocorr::AbstractVector, p::Integer, q=-1)
     end
 
     # Now treat the last estimate as the leading part of an MA(∞) model with coefficients ψ.
-    # Convert that model to the best mixed model ARMA(p,q). Again implied ψ[0]=1.
-    ψ = θ[end,:]
+    # Convert that model to the best mixed model ARMA(p,q). There's an implied ψ[0]=1.
+    ψ = θ[end,1:q+p]
     if p > 0
         Mrow1 = zeros(Float64, p)
         if q≥p
@@ -89,8 +90,83 @@ function innovations_estimate(autocorr::AbstractVector, p::Integer, q=-1)
 
     function psd(ω::Number)
         z = exp(1im*ω)
-        zbar = conj(z)
-        abs(pθ(z)*pθ(zbar)/(pϕ(z)*pϕ(zbar)))/π
+        abs2(pθ(z)/(pϕ(z)))/π
     end
     psd, pθ, pϕ
+end
+
+function fit_psd(noisecovar::AbstractVector, PSD::AbstractVector, pulsemodel::AbstractVector, p, q=-1;
+                 nmax=200, method=:Normal)
+    if q<0
+        q = p
+    end
+
+    N = length(PSD)
+    ω = LinRange(0, π, N)
+
+    if length(noisecovar) > nmax
+        noisecovar = noisecovar[1:nmax]
+    end
+    _, initθ, initϕ = innovations_estimate(noisecovar, p, q)
+
+    function model(z, param)
+        θ = Polynomial(param[1:q+1])
+        ϕ = Polynomial(vcat(1, param[q+2:end]))
+        abs2.(θ.(z)./(ϕ.(z)))/π
+    end
+
+    # function jacobian(ω, param)
+    #     J=zeros(Float64, length(ω), length(param))
+    #     z = exp.(1im*ω)
+    #     θ = Polynomial(param[1:q+1])
+    #     ϕ = Polynomial(vcat(1, param[q+2:end]))
+    #     numer = θ.(z)
+    #     denom = ϕ.(z)
+    #     for j=0:q
+    #         J[:,j+1] = (2param[j+1] + ???) ./denom
+    #     end
+    #     J
+    # end
+
+    window = 1.0 .- abs.(LinRange(-1, 1, length(pulsemodel)))
+    pulseFT2 = abs2.(rfft(pulsemodel.*window)/maximum(pulsemodel))
+    if N != length(pulseFT2)
+        throw(DimensionMismatch("fit_psd: length(PSD) $N != length(rfft(pulsemodel)) $(length(pulseFT2))"))
+    end
+
+    PSD = copy(PSD)
+    PSD[1] = 2PSD[2]-PSD[3]  # replace element 1 with linear extrapolation of 2:3.
+    if method == :Normal
+        ydata = PSD
+        weight = pulseFT2 ./ PSD.^3
+        fitmodel = model
+    elseif method == :Inverted
+        ydata  = 1.0 ./PSD
+        weight = pulseFT2 .* PSD
+        invmodel(z,param) = 1.0 ./model(z,param)
+        fitmodel = invmodel
+    elseif method == :Log
+        ydata = log.(PSD)
+        weight = pulseFT2 ./ PSD
+        logmodel(z,param) = log.(model(z,param))
+        fitmodel = logmodel
+    else
+        @assert method in (:Normal, :Inverted, :Log)
+    end
+    # Correct weight by the 1-pulse/true_noise/sum(pulse/true_noise)
+    κ = pulseFT2./PSD
+    κ /= sum(κ[2:end])
+    weight .*= (1 .- κ)
+    weight[1] = 0
+
+    z = exp.(1im*ω)
+    param0 = vcat(initθ[:], initϕ[1:end])
+    initθ *= sqrt(mean(PSD.*weight)/mean(model(z, param0).*weight))
+    param0 = vcat(initθ[:], initϕ[1:end])
+    @assert length(param0)==p+1+q
+    fit = curve_fit(fitmodel, z, ydata, weight, param0)
+    bestmodel(z) = model(z, fit.param)
+    model0(z) = model(z, param0)
+    ϕ = Polynomial(vcat(1,fit.param[q+2:end]))
+    fit, bestmodel, model0, param0, weight
 end
